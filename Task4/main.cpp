47334226b06aae9ff667691200a07e8384e8f341
OG Michael <6el0zu61979@mail.ru>
2024-06-30 17:33:32 +0700

    That was hard

--- Task4/main.cpp
+++ Task4/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int width;
+    int length;
+
+    printf("Enter width and length: \n");
+    cin >> width >> length;
+
+    for(int i = 0; i < length; i++){
+        if(i == 0){
+            printf("|");
+            for(int j = 0; j < width; j++){
+                printf("-");
+            }
+            printf("|\n");
+        }
+        if(i < length - 2) {
+            printf("|");
+            for (int d = 0; d < width; d++) {
+                printf(" ");
+            }
+            printf("|\n");
+        }
+        if(i == length - 1){
+            printf("|");
+            for(int j = 0; j < width; j++){
+                printf("-");
+            }
+            printf("|");
+        }
+    }
 }
